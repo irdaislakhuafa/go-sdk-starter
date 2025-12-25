09e9caa1760039295c537363c56e1643873cd05b
irdhaislakhuafa <irdhaislakhuafa@gmail.com>
2025-12-25 12:25:16 +0000

    feat: Add Nix configuration for development environment

---
+++ .idx/dev.nix
@@ -0,0 +1,87 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://firebase.google.com/docs/studio/customize-workspace
+{ pkgs, ... }: {
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.rustup
+
+    pkgs.go
+    pkgs.gopls
+
+    pkgs.bun
+    pkgs.nodejs
+    pkgs.prisma-engines
+    
+    pkgs.helix
+    pkgs.git
+    pkgs.gh
+    pkgs.glab
+    pkgs.fish
+    pkgs.gnumake
+    pkgs.sshed
+    pkgs.sshpass
+    pkgs.openssh
+    pkgs.openssl
+    pkgs.devbox
+    pkgs.devenv
+    pkgs.distrobox
+  ];
+
+  # Sets environment variables in the workspace
+  env = {};
+  idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
+    extensions = [
+      # "vscodevim.vim"
+      "bernabe.gemcommit"
+      "Codeium.codeium"
+      "esbenp.prettier-vscode"
+      "esdete.tailwind-rainbow"
+      "google.gemini-cli-vscode-ide-companion"
+      "google.geminicodeassist"
+      "Joundy.lumpat"
+      "ms-azuretools.vscode-containers"
+      "ms-azuretools.vscode-docker"
+      "stivo.tailwind-fold"
+    ];
+
+    # Enable previews
+    previews = {
+      enable = true;
+      previews = {
+        # web = {
+        #   # Example: run "npm run dev" with PORT set to IDX's defined port for previews,
+        #   # and show it in IDX's web preview panel
+        #   command = ["npm" "run" "dev"];
+        #   manager = "web";
+        #   env = {
+        #     # Environment variables to set for your server
+        #     PORT = "$PORT";
+        #   };
+        # };
+      };
+    };
+
+    # Workspace lifecycle hooks
+    workspace = {
+      # Runs when a workspace is first created
+      onCreate = {
+        # Example: install JS dependencies from NPM
+        # npm-install = "npm install";
+      };
+      # Runs when the workspace is (re)started
+      onStart = {
+        # Example: start a background task to watch and re-build backend code
+        # watch-backend = "npm run watch-backend";
+      };
+    };
+  };
+  services = {
+    docker = {
+      enable = true;
+    };
+  };
+}
